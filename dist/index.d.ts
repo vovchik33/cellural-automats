f6c1ea884fb44af1ef43878723bdd6e6e52d4d65
volodymyrkr <volodymyr.kravchenko.2000@gmail.com>
2019-12-17 00:24:59 +0200

    publish fixes

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export declare enum ConwayElementType {
-    BLOCK = 0,
-    GLIDER = 1,
-    LIGHTER = 2
-}
-export declare const mapConwayElementTypeToArray: (type: ConwayElementType) => number[] | number[][];
