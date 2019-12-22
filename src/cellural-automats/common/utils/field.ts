f2117aba9fdcf570564d9bce3021ab60965c5747
vovchik33 <vovchik33@gmail.com>
2019-12-22 04:44:17 +0200

    publish fixes

--- src/cellural-automats/common/utils/field.ts
+++ src/cellural-automats/common/utils/field.ts
@@ -1,52 +1,54 @@
-export type VectorType<T> = Array<T>;
+export type VectorType<T> = T[];
 export type FieldType<T> = VectorType<VectorType<T>>;
 
 export const createVector = <T>(length: number, initialValue: T): VectorType<T> => {
   const vector: T[] = new Array();
-  for(let i=0; i< length; i++) vector.push(initialValue);
+  for (let i = 0; i < length; i++) { vector.push(initialValue); }
   return vector;
-}
+};
 
-export const createField = <T>(width: number, height:number, initialValue: T): FieldType<T> => {
+export const createField = <T>(width: number, height: number, initialValue: T): FieldType<T> => {
   const field: FieldType<T> = new Array();
-  for(let i=0; i< height; i++) field.push(createVector(width, initialValue));
+  for (let i = 0; i < height; i++) { field.push(createVector(width, initialValue)); }
   return field;
-}
+};
 
 export const compareVectors = <T>(firstArray: VectorType<T>, secondArray: VectorType<T>): boolean => {
-  if (firstArray.length != secondArray.length) return false;
-  for(let i=0; i<firstArray.length; i++) {
-    if (firstArray[i] !== secondArray[i]) return false;
+  if (firstArray.length !== secondArray.length) { return false; }
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) { return false; }
   }
   return true;
-}
+};
 
 export const compareFields = <T>(firstField: FieldType<T>, secondField: FieldType<T>): boolean => {
-  if (firstField.length != secondField.length) return false;
-  for(let i=0; i<firstField.length; i++) {
-    if (!compareVectors(firstField[i], secondField[i])) return false;
+  if (firstField.length !== secondField.length) { return false; }
+  for (let i = 0; i < firstField.length; i++) {
+    if (!compareVectors(firstField[i], secondField[i])) { return false; }
   }
   return true;
-}
+};
 
 export const cloneVector = <T>(source: VectorType<T>, length?: number, initialValue?: T): VectorType<T> => {
   const result: VectorType<T> = new Array();
-  source.forEach((value) => {result.push(value)});
+  source.forEach((value) => {result.push(value); });
   if (length && initialValue !== undefined && length > source.length) {
-    for(let i=source.length; i<length; i++) {
+    for (let i = source.length; i < length; i++) {
       result.push(initialValue);
     }
   }
   return result;
-}
+};
 
-export const cloneField = <T>(source: FieldType<T>, width?: number, height?: number, initialValue?: T): FieldType<T> => {
+export const cloneField = <T>(
+    source: FieldType<T>, width?: number, height?: number, initialValue?: T,
+): FieldType<T> => {
   const result: FieldType<T> = new Array();
-  source.forEach((value:VectorType<T>) => {result.push(cloneVector(value, width, initialValue))});
+  source.forEach((value: VectorType<T>) => {result.push(cloneVector(value, width, initialValue)); });
   if (height && width && initialValue !== undefined && height > source.length) {
-    for(let i=source.length; i<height; i++) {
+    for (let i = source.length; i < height; i++) {
       result.push(createVector(width, initialValue));
     }
   }
   return result;
-}
+};
